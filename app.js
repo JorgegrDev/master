13aa1e4155c311a1b1b0708047bcaefb0a718a83
JorgeGR_dev <jorgegarza2003@hotmail.com>
2025-02-21 17:34:29 -0600

    establecer login como pantalla principal

---
+++ app.js
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        {/* Add other screens here */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
