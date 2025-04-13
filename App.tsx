c2655a26f48c620fbbad4b720201fa7898ea944f
JorgeGR_dev <jorgegarza2003@hotmail.com>
2025-04-13 14:30:03 -0600

    android config

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-import { Platform } from 'react-native';
-import { FC, PropsWithChildren } from 'react';
+import { Platform, View } from 'react-native';
+import { FC } from 'react';
 import './src/config/firebase';
 import { FirebaseInitializer } from './components/FirebaseInitializer';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 const App: FC = () => {
   return (
-    <FirebaseInitializer>
-      <>
-        {/* ...existing code... */}
-      </>
-    </FirebaseInitializer>
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
+      <FirebaseInitializer>
+        <View style={{ flex: 1 }}>
+          {/* Your app content will go here */}
+        </View>
+      </FirebaseInitializer>
+    </SafeAreaProvider>
   );
 }
 
